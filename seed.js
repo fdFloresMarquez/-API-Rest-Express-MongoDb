de1f0e50e30dccf8cb7f6e113f75fbc60280370a
Facundo <floresfacundo68@gmail.com>
2022-07-29 20:08:46 -0300

    commit all

---
+++ seed.js
@@ -0,0 +1,59 @@
+const Product = require('./models/product');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/farmStand')
+    .then(() => {
+        console.log('Mongo conectado!');
+    })
+    .catch(err => {
+        console.log('Ups hubo un error conectando Mongo...', err);
+    })
+
+// const p = new Product({
+//     name: 'Ruby Grapefruit',
+//     price: 1.99,
+//     category: 'fruit'
+// })
+// p.save()
+//     .then(p => {
+//         console.log(p)
+//     })
+//     .catch(err =>{
+//         console.log('Hubo un error en seeds!',err)
+//     })
+
+const seedProducts = [
+    {
+        name: 'Fairy Eggplant',
+        price: 1.00,
+        category: 'vegetable'
+    },
+    {
+        name: 'Organic Goddess Melon',
+        price: 4.99,
+        category: "fruit"
+    },
+    {
+        name: '0rganic Mini Seedless Waterme lon',
+        price: 3.99,
+        category: 'fruit'
+    },
+    {
+        name: 'Organic Celery',
+        price: 1.50,
+        category: 'vegetable'
+    },
+    {
+        name: 'Chocolate Whole Milk',
+        price: 2.69,
+        category: 'dairy'
+    }
+]
+
+Product.insertMany(seedProducts)
+    .then(res => {
+        console.log(res)
+    })
+    .catch(err => {
+        console.log(err)
+    })
